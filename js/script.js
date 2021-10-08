1319c850a9c23894a0777cb507681df4daa19839
Niraj Kafle <kafleniraj@gmail.com>
2021-10-08 18:59:08 +0545

    Other food items too

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const openRecipe = (food) => {
     window.open("https://www.foodnetwork.com/recipes/nepali-momo-4635126");
   }
 };
+
+
+const orderFood = (food) => {
+  if (food == 'momo'){
+    window.open("https://foodmandu.com/Restaurant/Index?q=momo&k=restaurant&cty=1");
+  }
+}
